3f44ddd0e21bf631b9365b6922ab684af36b5008
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-05-19 16:21:09 -0400

    create student directory page

---
+++ thomas-jefferson/src/routes/students.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/header'
+
+const Students = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>Student Directory</h1>
+        </div>
+    )
+}
+
+export default Students

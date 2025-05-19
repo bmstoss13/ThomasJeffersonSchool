78df43bbb3bf14c4d9c27ed9818c9cea59a5d6ca
aayushmap34 <hza9yx@virginia.edu>
2025-05-19 16:38:29 -0400

    create mock classes

--- thomas-jefferson/src/components/ClassDashboard.jsx
+++ thomas-jefferson/src/components/ClassDashboard.jsx
@@ -5,3 +5,11 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebase/config';
+
+const mockClasses = [
+    { teacher: 'Ms. Johnson', grade: 1, students: 24, room: 101 },
+    { teacher: 'Mr. Apple', grade: 2, students: 18, room: 221 },
+    { teacher: 'Mrs. Smith', grade: 3, students: 22, room: 113 },
+    { teacher: 'Ms. Lee', grade: 4, students: 28, room: 200 },
+    { teacher: 'Mrs. Jackson', grade: 5, students: 16, room: 50 },
+  ];

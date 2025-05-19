3e71bcac5fdeda625a34bfe5ecdb3eba8d86e5a5
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-05-19 16:12:16 -0400

    direct to dashboard first

--- thomas-jefferson/src/Router.jsx
+++ thomas-jefferson/src/Router.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Dashboard from "./routes/dashboard";
+import Login from "./routes/login";
 
 export const router = createBrowserRouter([
     { path: "/", element: <App/>},
     { path: "/dashboard", element: <Dashboard/>},
+    { path: "/login", element: <Login/>}
   ]);
--- thomas-jefferson/src/routes/login.jsx
+++ thomas-jefferson/src/routes/login.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../components/header'
 
 const login = () => {
     return (
         <div>
+            <Header/>
             <h1>login</h1>
         </div>
     )

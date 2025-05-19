bee9294193e109fd311c12cbbfbecd0a65baca53
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-05-19 16:52:35 -0400

    create list component for directories (will fix later when firebase is implemented)

--- thomas-jefferson/src/components/list.jsx
+++ thomas-jefferson/src/components/list.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-const list = () => {
+const list = ({items, onEdit, onDelete}) => {
+    
     return (
         <div>
+            <ul>
+                {items.map((item) => (
+                    <li key={item.id}>
+                        <span>{item.name}</span>
+                        <div>
+                            <button onClick={() => onEdit(item)}>Edit</button>
+                            <button onClick={() => onDelete(item.id)}>Delete</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }

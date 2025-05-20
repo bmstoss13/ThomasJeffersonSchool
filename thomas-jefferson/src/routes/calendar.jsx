5d9d6bad6d511d808b2aff3c22cea0951c5f6632
3amBEANS <ha.aidenS@gmail.com>
2025-05-20 17:20:55 -0400

    calendar ui

--- thomas-jefferson/src/routes/calendar.jsx
+++ thomas-jefferson/src/routes/calendar.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
-import Header from '../components/header'
+import React from 'react';
+import Header from '../components/header';
+import {ScheduleXCalendar, useCalendarApp} from "@schedule-x/react";
+import {createViewWeek, createViewMonthGrid} from '@schedule-x/calendar';
+import '@schedule-x/theme-default/dist/calendar.css'
+import { createEventModalPlugin } from '@schedule-x/event-modal';
+import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop';
 
 
-const calendar = () => {
+function calendar(){
 
-  return (
+    const today = new Date();
+    const formattedToday = today.toISOString().split('T')[0];
+    console.log(formattedToday);
+    const calendar = useCalendarApp({
+      views:[
+        createViewWeek(),
+        createViewMonthGrid()
+      ],
+      events: [
+        {
+          id: 1, 
+          title: 'My new event',
+          description: 'this is my event description',
+          start: '2025-01-01 00:00',
+          end: '2025-01-01 02:00'
+        }
+      ],
+      selectedDate: formattedToday,
+      plugins:[
+        createEventModalPlugin(),
+        createDragAndDropPlugin()
+      ]
+    })
+
+  return(
     <>
+      <div>
         <Header/>
-        <div>
-            <p>This is a calendar</p> 
-            
-        </div>
+        <div style={{ marginBottom: '60px' }}> </div> {/* <-- added this for spacing, couldn't think of a better way*/}
+        <button >Create Event</button>
+        <ScheduleXCalendar calendarApp={calendar}/>
+        
+      </div>
     </>
   )
 }
 
-export default calendar
+export default calendar;

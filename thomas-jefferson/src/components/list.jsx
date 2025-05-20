01eb8a434a44cc696392f121cc88fec77ad5eea4
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-05-20 16:03:34 -0400

    styling to lists

--- thomas-jefferson/src/components/list.jsx
+++ thomas-jefferson/src/components/list.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import '../styles/list.css'
 
 const List = ({items, onEdit, onDelete}) => {
     
     return (
-        <div>
-            <ul>
+        <div className="list-container">
+            <ul className='list-items'>
                 {items.map((item) => (
-                    <li key={item.id}>
-                        <span>{item.last_name}{", "}{item.first_name}</span>
-                        <div>
-                            <button onClick={() => onEdit(item)}>Edit</button>
-                            <button onClick={() => onDelete(item.id)}>Delete</button>
+                    <li key={item.id} className="list-item">
+                        <span className="item-name">{item.last_name}{", "}{item.first_name}</span>
+                        <div className="button-container">
+                            <button className="edit-button" onClick={() => onEdit(item)}>Edit</button>
+                            <button className="delete-button" onClick={() => onDelete(item.id)}>Delete</button>
                         </div>
                     </li>
                 ))}

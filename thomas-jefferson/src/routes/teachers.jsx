a212942f0ef844ba01007ba81d21693b142044e7
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-05-20 15:26:04 -0400

    display teachers

--- thomas-jefferson/src/routes/teachers.jsx
+++ thomas-jefferson/src/routes/teachers.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/header'
+import List from '../components/list'
+import { getAllTeachers, deleteTeacher, updateTeacher, } from "../utils/CRUDteachers"
 
 const Teachers = () => {
+    const [teachers, setTeachers] = useState([])
+
+    const fetchTeachers = async() => {
+        const data = await getAllTeachers();
+        setTeachers(data);
+        //firebase logic
+    }
+    const handleEdit = async(id) => {
+        await deleteTeacher(id);
+        fetchTeachers();
+        //firebase logic
+    }
+    const handleDelete = (teacher) => {
+        console.log("editing", teacher)
+        //firebase logic
+    }
+
+    useEffect(() => {
+        fetchTeachers()
+    }, []);
     return (
         <div>
             <Header/>
             <h1>Teacher Directory</h1>
+            <List
+                items={teachers}
+                onEdit={handleEdit}
+                onDelete={handleDelete}
+            />
         </div>
     )
 }

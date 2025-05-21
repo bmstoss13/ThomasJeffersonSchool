4816df051ca1e87fd4fb4edd12ee58f6283a2e07
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-05-21 10:09:25 -0400

    directory clean up

--- thomas-jefferson/src/routes/students.jsx
+++ thomas-jefferson/src/routes/students.jsx
@@ -3,9 +3,12 @@ import Header from '../components/header'
 import List from '../components/list'
 import { getAllStudents, deleteStudent, updateStudent, } from "../utils/CRUDstudents"
 import '../styles/students.css'
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const Students = () => {
     const [students, setStudents] = useState([])
+    const nav = useNavigate()
 
     const fetchStudents = async () => {
         const data = await getAllStudents();
@@ -29,6 +32,7 @@ const Students = () => {
 
         <div className="students-container">
             <Header/>
+            <Button className="adminButton" variant="contained" color="success" onClick={() => nav('/students/new')}>+ Add New Student</Button>
             <div className="students-content">
             <h1 className="students-name">Student Directory</h1>
             <List

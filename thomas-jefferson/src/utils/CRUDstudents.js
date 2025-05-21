f07b8e8f8f13f1f6c29963b22bedd541f05c953d
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-05-21 12:23:50 -0400

    try catch added to CRUDstudent.js

--- thomas-jefferson/src/utils/CRUDstudents.js
+++ thomas-jefferson/src/utils/CRUDstudents.js
@@ -4,31 +4,64 @@ import { collection, getDoc, getDocs, addDoc, updateDoc, deleteDoc, doc, onSnaps
 const studentRef = collection(db, "students");
 
 export const getAllStudents = async () => {
-  const data = await getDocs(studentRef)
-  const students = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-  return students.sort((a, b) => {
-    const lastNameComparison = a.last_name.localeCompare(b.last_name)
-    return lastNameComparison !== 0 ? lastNameComparison : a.first_name.localeCompare(b.first_name)
-  })
+
+  try{
+    const data = await getDocs(studentRef)
+    const students = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+    return students.sort((a, b) => {
+      const lastNameComparison = a.last_name.localeCompare(b.last_name)
+      return lastNameComparison !== 0 ? lastNameComparison : a.first_name.localeCompare(b.first_name)
+    })
+  }
+  catch(e){
+    console.log("There was an error while trying to retrieve all students:", e)
+  }
+
 };
 
 export const getStudent = async (id) => {
-  const studentDoc = doc(db, "students", id);
-  const snap = await getDoc(studentDoc);
-  return snap.exists() ? { ...snap.data(), id: snap.id } : null;
+
+  try{
+    const studentDoc = doc(db, "students", id);
+    const snap = await getDoc(studentDoc);
+    return snap.exists() ? { ...snap.data(), id: snap.id } : null;
+  }
+  catch(e){
+    console.log("There was an error while trying to retrieve student:", e)
+  }
+
 };
 
-
 export const addStudent = async (student) => {
-  return await addDoc(studentRef, student)
+
+  try{
+    return await addDoc(studentRef, student)
+  }
+  catch(e){
+    console.log("There was an error while trying to add student:", e)
+  }
+
 };
 
 export const updateStudent = async (id, updatedInfo) => {
-  const studentDoc = doc(db, "students", id)
-  return await updateDoc(studentDoc, updatedInfo)
+
+  try{
+    const studentDoc = doc(db, "students", id)
+    return await updateDoc(studentDoc, updatedInfo)
+  }
+  catch(e){
+    console.log("There was an error while trying to update the student's information", e)
+  }
 };
 
 export const deleteStudent = async (id) => {
-  const studentDoc = doc(db, "students", id)
-  return await deleteDoc(studentDoc)
+
+  try{
+    const studentDoc = doc(db, "students", id)
+    return await deleteDoc(studentDoc)
+  }
+  catch(e){
+    console.log("There was an error while trying to delete the student's data:", e)
+  }
+
 };

90eddff42f38b3ed7f413c8a90fd0daefc612929
aayushmap34 <hza9yx@virginia.edu>
2025-05-21 10:59:07 -0400

    create CRUD for classes

---
+++ thomas-jefferson/src/utils/CRUDclasses.js
@@ -0,0 +1,15 @@
+import { collection, getDocs, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase"; 
+
+export const getAllClasses = async () => {
+    const querySnapshot = await getDocs(collection(db, "classes"));
+    return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+};
+
+export const deleteClass = async (id) => {
+    await deleteDoc(doc(db, "classes", id));
+};
+
+export const updateClass = async (id, data) => {
+    await updateDoc(doc(db, "classes", id), data);
+};

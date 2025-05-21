a54b662a5f8eecd01a58dce1f083ccb432581239
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-05-21 11:41:21 -0400

    student search bar working

---
+++ thomas-jefferson/src/components/StudentSearchBar.jsx
@@ -0,0 +1,18 @@
+// import React, { useState, useEffect } from 'react';
+// import { db } from '../firebase';
+// import { getAllStudents } from '../utils/CRUDstudents';
+
+// const StudentSearchBar = () => {
+//     const 
+
+//     function search(e) {
+//         e.preventDefault()
+//         setQ(e.target.value)
+//     }
+//     return (
+//         <div>
+//         </div>
+//     );
+// };
+
+// export default StudentSearchBar;

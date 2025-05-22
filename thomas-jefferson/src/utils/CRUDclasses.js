09a59c62267dd8f180897a4cdfa6850a8ab15abc
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-05-22 12:35:16 -0400

    classes logic in student form

--- thomas-jefferson/src/utils/CRUDclasses.js
+++ thomas-jefferson/src/utils/CRUDclasses.js
@@ -4,6 +4,7 @@ import { db } from "../firebase";
 export const getAllClasses = async () => {
     const querySnapshot = await getDocs(collection(db, "classes"));
     return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    
 };
 
 export const deleteClass = async (id) => {

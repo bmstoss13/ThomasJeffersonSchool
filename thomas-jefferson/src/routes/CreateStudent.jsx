2f816756377d2d12d8b8ca174f0567f55d3ddfa0
bmstoss13 <123670110+bmstoss13@users.noreply.github.com>
2025-05-22 18:12:43 -0400

    add students to classes

--- thomas-jefferson/src/routes/CreateStudent.jsx
+++ thomas-jefferson/src/routes/CreateStudent.jsx
@@ -1,14 +1,48 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import StudentForm from '../components/StudentForm';
 import { addStudent } from '../utils/CRUDstudents';
 import Header from '../components/header';
+import { updateClass, getClassById } from '../utils/CRUDclasses';
+import { doc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 export default function CreateStudent() {
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const classId = searchParams.get('classId');
 
   const handleCreate = async (data) => {
-    await addStudent(data);
-    navigate(-1);
+    try{
+      if (classId) {
+        data.class_id = classId;
+      }
+
+      const studentRef = await addStudent(data)
+      if (classId && studentRef) {
+        const classData = await getClassById(classId);
+        if(classData) {
+          const existingStudents = classData.student || [];
+          const studentRefPath = `students/${studentRef.id}`
+          const updatedStudentRefs = [...existingStudents, studentRefPath]
+          await updateClass(classId, { student: updatedStudentRefs })
+
+        }
+      }
+
+      if(classId) {
+        navigate(`/class/${classId}`)
+      }
+
+      else{
+        navigate(-1);
+      }
+    } 
+    catch(e){
+      console.error("Error creating student:", e)
+
+    }
+    // await addStudent(data);
+    // navigate(-1);
   };
 
   return (
@@ -16,7 +50,7 @@ export default function CreateStudent() {
       <Header/>
       {/* i want to center the following student form */}
       <div style={{ padding: '2rem', marginTop: '11rem'}}>
-        <h2>Create New Student</h2>
+        <h2>{classId ? 'Add Student to Class' : 'Create New Student'}</h2>
         <StudentForm onSubmit={handleCreate} />
       </div>
     </>
